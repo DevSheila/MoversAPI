d5c8746d46adfba81f7eaac27750abceef394f86
Sheila Sharon <devsheilawambui@gmail.com>
2021-08-03 20:12:44 +0300

    add read and delete sql methods to sql2o

--- src/main/java/models/dao/Sql2oMovingOrdersDao.java
+++ src/main/java/models/dao/Sql2oMovingOrdersDao.java
@@ -5,6 +5,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oMovingOrdersDao implements MovingOrdersDao {
@@ -30,21 +31,48 @@ public class Sql2oMovingOrdersDao implements MovingOrdersDao {
 
     @Override
     public List<MovingOrders> getMovingOrderByMovingCompany(String moving_company) {
-        return null;
+        List<MovingOrders> companyMovingOrders ;
+
+        try (Connection con = sql2o.open()) {
+            companyMovingOrders = con.createQuery("SELECT * FROM moving_orders WHERE moving_company = :moving_company")
+                    .addParameter("moving_company", moving_company)
+                    .executeAndFetch(MovingOrders.class);
+        }
+        return companyMovingOrders;
     }
 
     @Override
     public List<MovingOrders> getMovingOrderByUserName(String userName) {
-        return null;
+        List<MovingOrders> userMovingOrders;
+
+        try (Connection con = sql2o.open()) {
+            userMovingOrders = con.createQuery("SELECT * FROM moving_orders WHERE userName = :userName")
+                    .addParameter("userName", userName)
+                    .executeAndFetch(MovingOrders.class);
+        }
+        return userMovingOrders;
+
     }
 
     @Override
     public MovingOrders findById(int id) {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM moving_orders WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(MovingOrders.class);
+        }
     }
 
     @Override
     public void deleteMovingOrderById(int id) {
+        String sql = "DELETE from moving_orders WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
 
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 }
